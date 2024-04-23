ad81782b89f0a82bc375c0dd9e874af4b0a52991
sjourdbeans <sjoerdbuitjes@gmail.com>
2024-04-23 15:58:38 +0200

    Added assignment 1.2 and finished final assignment

--- employee_processing.py
+++ employee_processing.py
@@ -1,3 +1,14 @@
-class Process:
-    def __init__(self,file_path):
-        self.file=file_path
+import pandas as pd
+from employee import Employee
+
+class Processing:
+    def __init__(self,filename):
+        self.path=f"data/{filename}"
+        self.data=pd.read_csv(self.path)
+        self.name=None
+        self.birthday=None
+        self.id=None
+    
+    def employee_list(self):
+        pass
+
