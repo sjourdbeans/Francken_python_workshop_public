8485aa41de2e103972209dae59f4c5c2bc81b5fe
sjourdbeans <sjoerdbuitjes@gmail.com>
2024-04-23 16:25:42 +0200

    removed data folder and put it with other files

--- employee_processing.py
+++ employee_processing.py
@@ -3,7 +3,7 @@ from employee import Employee
 
 class Processing:
     def __init__(self,filename):
-        self.path=f"data/{filename}"
+        self.path=f"{filename}"
         self.data=pd.read_csv(self.path)
         self.name=None
         self.birthday=None
